d249e04a8719bf88b19cbf186067e8937d2a7237
mobinkazemi <s.mobinkazemi@gmail.com>
2022-01-13 21:01:26 +0330

    changed port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3100);
 }
 bootstrap();
