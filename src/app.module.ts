9c914a49c1989a4fa4ffb11d5ead12c8b8f5a93b
mobinkazemi <s.mobinkazemi@gmail.com>
2022-01-13 21:00:56 +0330

    removed typeOrm configurations form app.modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import {Table, Column, Model, DataType } from 'sequelize-typescript'
 
 @Module({
-  imports: [
-    TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '2122',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-  ],
+  imports: [TasksModule],
 })
 export class AppModule {}
