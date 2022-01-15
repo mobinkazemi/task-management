8d100229fcf821f92a8de0472ecd02326ebb34f1
mobinkazemi <s.mobinkazemi@gmail.com>
2022-01-15 13:24:52 +0330

    added id to updateTaskDTO

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -2,6 +2,7 @@ import { IsEnum, IsNotEmpty } from 'class-validator';
 import { TaskStatus } from '../task-status.enum';
 
 export class UpdateTaskDTO {
+  id: number;
   @IsEnum(TaskStatus)
   status: TaskStatus;
 }
