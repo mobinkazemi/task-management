00b5ae99f637080a5e3d7a2ee76d7fcdc60e2892
mobinkazemi <s.mobinkazemi@gmail.com>
2022-01-17 20:17:11 +0330

    replaced hard-coded configs with .env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from './config.schema';
 @Module({
-  imports: [TasksModule, UsersModule],
+  imports: [
+    TasksModule,
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${process.env.STAGE}`],
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
 })
 export class AppModule {}
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,17 +1,20 @@
 import { Sequelize } from 'sequelize-typescript';
 import { User } from 'src/users/users.entity';
 import { Task } from '../tasks/tasks.entity.sequelize';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
-    useFactory: async () => {
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => {
       const sequelize = new Sequelize({
         dialect: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: '2122',
-        database: 'task-management',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
       });
       sequelize.addModels([Task, User]);
       await sequelize.sync();
